7cb1f4fa365c761906b57eaab2ba9befa96e8280
Falguni Joshi <joshiphalgun@gmail.com>
2026-07-13 00:31:33 +0530

    Complete operators practice

---
+++ 01_Basics/operators.py
@@ -0,0 +1,35 @@
+# ==========================================
+# PYTHON OPERATORS
+# ==========================================
+
+a = 10
+b = 5
+
+# Arithmetic Operators
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Floor Division =", a // b)
+print("Modulus =", a % b)
+print("Exponent =", a ** b)
+
+print("--------------------------------")
+
+# Comparison Operators
+print("a > b :", a > b)
+print("a < b :", a < b)
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
+
+print("--------------------------------")
+
+# Logical Operators
+x = True
+y = False
+
+print("x and y =", x and y)
+print("x or y =", x or y)
+print("not x =", not x)
